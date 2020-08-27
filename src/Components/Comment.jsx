cb6b344496532a0270398a35ae6cfbd8e1b6e58e
Dukenfox <wonyoung989@gmail.com>
2020-08-27 20:57:00 +0200

    sldkj

---
+++ src/Components/Comment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+></link>;
+
+function Comment() {
+  return <div>
+
+
+  </div>;
+}
+export default Comment 
